df73fde9a88ad3acb623ea59e945ccd2a2847509
povlix <146538835+povlix@users.noreply.github.com>
2024-12-03 16:54:10 +0100

    Add files via upload

---
+++ Lab06/Lab06/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    internal class Animal
+    {
+        public string Species { get; set; }
+        public string Name { get; set; }
+    }
+}
